f2d84eb90792b2656c657c5b0b49981c325df52f
Dominik Pokrzywa <dominik.pokrzywa@essyo.pl>
2024-03-19 13:39:44 +0100

    Number generator tests

--- src/main/java/pl/pwr/ite/niduc/Main.java
+++ src/main/java/pl/pwr/ite/niduc/Main.java
@@ -8,8 +8,6 @@ public class Main {
     private static NumberGenerator numberGenerator = new NumberGeneratorImpl();
 
     public static void main(String[] args) {
-        for(int i = 0; i < 100; i++) {
-            System.out.println(numberGenerator.nextInteger(5, 100));
-        }
+        
     }
 }
---
+++ src/test/java/NumberGeneratorTest.java
@@ -0,0 +1,56 @@
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import pl.pwr.ite.niduc.service.NumberGenerator;
+import pl.pwr.ite.niduc.service.impl.NumberGeneratorImpl;
+
+import java.lang.annotation.Repeatable;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class NumberGeneratorTest {
+
+    private NumberGenerator numberGenerator;
+
+    private final double[] constants = {
+            0.4254, 0.2944, 0.2487, 0.2148, 0.1870, 0.1630, 0.1415,
+            0.1219, 0.1036, 0.0862, 0.0697, 0.0537, 0.0381, 0.0227, 0.0076
+    };
+
+    @BeforeEach
+    void setup() {
+        numberGenerator = new NumberGeneratorImpl();
+    }
+
+    @RepeatedTest(10)
+    public void numberGeneratorNormalDistributionTest() {
+        int[] numbers = IntStream.generate(numberGenerator::nextInteger).limit(30).sorted().toArray();
+       double calculatedSum = 0;
+        var n = (int) Math.floor(numbers.length / 2);
+        for(int i = 0; i < n; i++) {
+            var a = numbers[i] - numbers[numbers.length - 1 - i];
+            calculatedSum += constants[i] * a;
+        }
+        var w = calculateW(calculatedSum, numbers);
+
+        assertTrue(w > 0.900);
+    }
+
+    private double calculateAverage(int[] numbers) {
+        return Arrays.stream(numbers).average().orElse(0);
+    }
+
+    private double calculateW(double sum, int[] numbers) {
+        return (sum*sum)/(calculateDeviation(calculateAverage(numbers), numbers));
+    }
+
+    private double calculateDeviation(double average, int[] numbers) {
+        return Arrays.stream(numbers)
+                .mapToDouble(i -> Math.pow(average - i, 2))
+                .sum();
+    }
+}
