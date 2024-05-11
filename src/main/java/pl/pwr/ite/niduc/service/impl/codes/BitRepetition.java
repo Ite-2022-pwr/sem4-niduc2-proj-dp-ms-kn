1e479c426507d311bbc25bc6b7041f3abacbceee
gosiula <gaskowron4@gmail.com>
2024-05-11 16:01:40 +0200

    gotowy program!!!!!!

---
+++ src/main/java/pl/pwr/ite/niduc/service/impl/codes/BitRepetition.java
@@ -0,0 +1,188 @@
+package pl.pwr.ite.niduc.service.impl.codes;
+
+import pl.pwr.ite.niduc.service.impl.channels.BSC;
+import pl.pwr.ite.niduc.service.impl.channels.GilbertElliot;
+import pl.pwr.ite.niduc.service.impl.generator.NumberGeneratorImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BitRepetition {
+
+    // Metoda zamieniająca ciąg znaków na binarną listę
+    public static List<Integer> strToBin(String string) {
+        List<Integer> ret = new ArrayList<>();
+        for (char c : string.toCharArray()) {
+            String binary = Integer.toBinaryString(c);
+            while (binary.length() < 7) {
+                binary = "0" + binary; // Uzupełnienie zerami, aby uzyskać stałą długość bloku kodowego
+            }
+            for (char bit : binary.toCharArray()) {
+                ret.add(Character.getNumericValue(bit));
+            }
+        }
+        return ret;
+    }
+
+
+    // Metoda zamieniająca binarną listę na ciąg znaków
+    public static String binToStr(List<Integer> binary) {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < binary.size(); i += 7) {
+            StringBuilder binaryChar = new StringBuilder();
+            for (int j = i; j < i + 7; j++) {
+                if (j < binary.size()) {
+                    binaryChar.append(binary.get(j));
+                }
+            }
+            int charCode = Integer.parseInt(binaryChar.toString(), 2);
+            output.append((char) charCode);
+        }
+        return output.toString();
+    }
+
+
+    // Metoda kodująca wiadomość przy użyciu kodu powielania bitów
+    public static List<Integer> encode(List<Integer> msg, int repetitionFactor) {
+        List<Integer> encodedMsg = new ArrayList<>();
+        for (int bit : msg) {
+            // Powielenie każdego bitu wiadomości
+            for (int i = 0; i < repetitionFactor; i++) {
+                encodedMsg.add(bit);
+            }
+        }
+        return encodedMsg;
+    }
+
+
+    // Metoda dekodująca zakodowaną wiadomość przy użyciu kodu powielania bitów
+    public static List<Integer> decode(List<Integer> msg, int repetitionFactor) {
+        List<Integer> decodedMsg = new ArrayList<>();
+        for (int i = 0; i < msg.size(); i += repetitionFactor) {
+            int zeros = 0;
+            int ones = 0;
+            for (int j = i; j < i + repetitionFactor; j++) {
+                if (j < msg.size()) {
+                    if (msg.get(j) == 0) {
+                        zeros++;
+                    } else {
+                        ones++;
+                    }
+                }
+            }
+            // Sprawdzenie większości bitów
+            int decodedBit = (zeros > ones) ? 0 : 1;
+            decodedMsg.add(decodedBit);
+        }
+        return decodedMsg;
+    }
+
+
+
+    // Metoda przekazująca zakodowaną wiadomość przez kanał BSC
+    public static List<Integer> transmitBsc(List<Integer> encodedMsg, BSC bsc) {
+        // Konwersja listy na tablicę intów
+        int[][] encodedArray = new int[1][encodedMsg.size()];
+        for (int i = 0; i < encodedMsg.size(); i++) {
+            encodedArray[0][i] = encodedMsg.get(i);
+        }
+
+        // Przekazanie zakodowanej wiadomości do kanału BSC
+        int[][] transmittedArray = bsc.transmit(encodedArray);
+
+        // Konwersja z powrotem do listy
+        List<Integer> transmittedList = new ArrayList<>();
+        for (int bit : transmittedArray[0]) {
+            transmittedList.add(bit);
+        }
+
+        return transmittedList;
+    }
+
+
+    // Metoda przekazująca zakodowaną wiadomość przez kanał Gilberta-Elliota
+    public static List<Integer> transmitGe(List<Integer> encodedMsg, GilbertElliot ge) {
+        // Konwersja listy na tablicę intów
+        int[][] encodedArray = new int[1][encodedMsg.size()];
+        for (int i = 0; i < encodedMsg.size(); i++) {
+            encodedArray[0][i] = encodedMsg.get(i);
+        }
+
+        // Przekazanie zakodowanej wiadomości do kanału Gilberta-Elliota
+        int[][] transmittedArray = ge.transmit(encodedArray);
+
+        // Konwersja z powrotem do listy
+        List<Integer> transmittedList = new ArrayList<>();
+        for (int bit : transmittedArray[0]) {
+            transmittedList.add(bit);
+        }
+
+        return transmittedList;
+    }
+
+    public static void main(String[] args) {
+        // Parameter powtarzania bitów podczas enkodowania
+        int repetitonFactor = 3;
+
+        // Wiadomość do przesłania
+        String message = "this is important information that should be kept secret";
+        System.out.println("Original Message - string:");
+        System.out.println(message);
+        System.out.println();
+
+        // Wiadomość do przesłania w bitach
+        List<Integer> binMessage = strToBin(message);
+        System.out.println("Original Message:");
+        System.out.println(binMessage);
+        System.out.println();
+
+        // Zakodowanie wiadomości za pomocą kodu Hamminga (7,4)
+        List<Integer> encodedMsg = encode(binMessage, repetitonFactor);
+        System.out.println("Encoded Message:");
+        System.out.println(encodedMsg);
+        System.out.println();
+
+        // Inicjalizacja parametrów kanału
+        double ber = 0.02;
+        double pOfErrorWhenGood = 0.01;
+        double pOfGoodToBad = 0.1;
+        double pOfErrorWhenBad = 0.1;
+        double pOfBadToGood = 0.8;
+
+        // Symulacja transmisji przez wybrany kanał
+        String channel = "BSC";
+        List<Integer> transmittedMsg = switch (channel) {
+            case "BSC" -> {
+                // Symulacja transmisji przez kanał BSC
+                BSC bsc = new BSC(ber, new NumberGeneratorImpl());
+                yield transmitBsc(encodedMsg, bsc);
+            }
+            case "GE" -> {
+                // Symulacja transmisji przez kanał Gilberta-Elliota
+                GilbertElliot ge = new GilbertElliot(pOfErrorWhenGood, pOfGoodToBad, pOfErrorWhenBad, pOfBadToGood, new NumberGeneratorImpl());
+                yield transmitGe(encodedMsg, ge);
+            }
+            case "null" -> encodedMsg;
+            default -> null;
+        };
+
+        // Wiadomość po transmisji
+        System.out.println("Transmitted message:");
+        System.out.println(transmittedMsg);
+        System.out.println();
+
+        // Dekodowanie wiadomości połączone z poprawianiem jej
+        List<Integer> decodedMessage = decode(transmittedMsg, repetitonFactor);
+
+        // Wydrukowanie zdekodowanej wiadomości
+        System.out.println("Decoded and corrected message:");
+        System.out.println(decodedMessage);
+        System.out.println();
+
+        // Konwersja listy binarnej na ciąg znaków - ostateczna wiadomość końcowa
+        String decodedStr = binToStr(decodedMessage);
+        System.out.println("Decoded message - string:");
+        System.out.println(decodedStr);
+        System.out.println();
+    }
+}
--- src/main/java/pl/pwr/ite/niduc/service/impl/simulation/SimulationBitRepetition.java
+++ src/main/java/pl/pwr/ite/niduc/service/impl/simulation/SimulationBitRepetition.java
@@ -1,5 +1,6 @@
 package pl.pwr.ite.niduc.service.impl.simulation;
 
+import pl.pwr.ite.niduc.service.impl.codes.BitRepetition;
 import pl.pwr.ite.niduc.service.impl.codes.Hamming;
 import pl.pwr.ite.niduc.service.impl.channels.BSC;
 import pl.pwr.ite.niduc.service.impl.channels.GilbertElliot;
@@ -8,9 +9,12 @@ import pl.pwr.ite.niduc.service.impl.generator.NumberGeneratorImpl;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SimulationHamming {
+public class SimulationBitRepetition {
 
     public static void main(String[] args) {
+        // Parameter powtarzania bitów podczas enkodowania
+        int repetitonFactor = 3;
+
         // Definicja wiadomości do przesłania
         String message = "this is important information that should be kept secret";
 
@@ -30,77 +34,60 @@ public class SimulationHamming {
         // Inicjalizacja sum dla obliczenia średnich
         int totalBitsAfterTransmission = 0;
         int totalBitsAfterCorrection = 0;
-        int totalMessageAfterTransmission = 0;
         int totalMessageAfterCorrection = 0;
         double totalPercentBitsCorrection = 0;
-        double totalPercentCharsCorrection = 0;
         double totalPercentBitsError = 0;
         double totalPercentCharsError = 0;
 
         // Powtórzenie symulacji
         for (int i = 0; i < numSimulations; i++) {
             // Symulacja
-            List<Integer> results = simulate(message, channel, ber, pOfErrorWhenGood, pOfGoodToBad, pOfErrorWhenBad, pOfBadToGood);
+            List<Integer> results = simulate(message, repetitonFactor, channel, ber, pOfErrorWhenGood, pOfGoodToBad, pOfErrorWhenBad, pOfBadToGood);
 
             // Aktualizacja sum
             totalBitsAfterTransmission += results.get(0);
             totalBitsAfterCorrection += results.get(1);
-            totalMessageAfterTransmission += results.get(2);
-            totalMessageAfterCorrection += results.get(3);
-            totalPercentBitsCorrection += results.get(4);
-            totalPercentCharsCorrection += results.get(5);
-            totalPercentBitsError += results.get(6);
-            totalPercentCharsError += results.get(7);
+            totalMessageAfterCorrection += results.get(2);
+            totalPercentBitsCorrection += results.get(3);
+            totalPercentBitsError += results.get(4);
+            totalPercentCharsError += results.get(5);
         }
 
         // Obliczenie średnich
         double avgBitsAfterTransmission = (double) totalBitsAfterTransmission / numSimulations;
         double avgBitsAfterCorrection = (double) totalBitsAfterCorrection / numSimulations;
-        double avgMessageAfterTransmission = (double) totalMessageAfterTransmission / numSimulations;
         double avgMessageAfterCorrection = (double) totalMessageAfterCorrection / numSimulations;
         double avgPercentBitsCorrection = totalPercentBitsCorrection / numSimulations;
-        double avgPercentCharsCorrection = totalPercentCharsCorrection / numSimulations;
         double avgPercentBitsError = totalPercentBitsError / numSimulations;
         double avgPercentCharsError = totalPercentCharsError / numSimulations;
 
         // Wyświetlenie średnich
         System.out.println("Average Swapped bits after Transmission: " + avgBitsAfterTransmission);
         System.out.println("Average Swapped bits after Correction: " + avgBitsAfterCorrection);
-        System.out.println("Average Swapped chars after Transmission: " + avgMessageAfterTransmission);
         System.out.println("Average Swapped chars after Correction: " + avgMessageAfterCorrection);
         System.out.printf("Average Percent bits correction: %.2f%%\n", avgPercentBitsCorrection);
-        System.out.printf("Average Percent chars correction: %.2f%%\n", avgPercentCharsCorrection);
         System.out.printf("Average Percent bits error: %.2f%%\n", avgPercentBitsError);
         System.out.printf("Average Percent chars error: %.2f%%\n", avgPercentCharsError);
     }
 
-    public static List<Integer> simulate(String originalMsgString, String channel, double ber, double pOfErrorWhenGood, double pOfGoodToBad, double pOfErrorWhenBad, double pOfBadToGood) {
+    public static List<Integer> simulate(String originalMsgString, int repetitonFactor, String channel, double ber, double pOfErrorWhenGood, double pOfGoodToBad, double pOfErrorWhenBad, double pOfBadToGood) {
         // Zapisanie wiadomości do przesłania w bitach
-        List<Integer> originalMsgBin = Hamming.strToBin(originalMsgString);
+        List<Integer> originalMsgBin = BitRepetition.strToBin(originalMsgString);
 
         // Enkodowanie wiadomości
-        List<Integer> encodedMsgBin = Hamming.encode(originalMsgBin);
+        List<Integer> encodedMsgBin = BitRepetition.encode(originalMsgBin, repetitonFactor);
 
         // Przesyłanie wiadomości przez wybrany kanał
         List<Integer> transmittedMsgBin = transmit(channel, encodedMsgBin, ber, pOfErrorWhenGood, pOfGoodToBad, pOfErrorWhenBad, pOfBadToGood);
 
-        // Wiadomość jako String po transmisji przez kanał
-        String transmittedMsgString = Hamming.binToStr(Hamming.decode(transmittedMsgBin));
-
-        // Obliczenie syndromu błędu
-        List<Integer> syndrome = Hamming.syndrome(transmittedMsgBin);
-
-        // Korekcja błędów
-        List<Integer> correctedMsgBin = Hamming.correct(syndrome, transmittedMsgBin);
-
-        // Dekodowanie poprawionej wiadomości
-        List<Integer> decodedMsgBin = Hamming.decode(correctedMsgBin);
+        // Dekodowanie wiadomości połączone z poprawianiem jej
+        List<Integer> decodedMsgBin = BitRepetition.decode(transmittedMsgBin, repetitonFactor);
 
         // Zapisanie wiadomości po dekodowaniu jako string
         String decodedMsgString = Hamming.binToStr(decodedMsgBin);
 
         // Analiza wyników
-        return analyzeResults(originalMsgString, originalMsgBin, encodedMsgBin, transmittedMsgBin, transmittedMsgString, decodedMsgBin, decodedMsgString);
+        return analyzeResults(originalMsgString, originalMsgBin, encodedMsgBin, transmittedMsgBin, decodedMsgBin, decodedMsgString);
     }
 
     public static List<Integer> transmit(String channel, List<Integer> encodedMsg, double ber, double pOfErrorWhenGood, double pOfGoodToBad, double pOfErrorWhenBad, double pOfBadToGood) {
@@ -120,24 +107,18 @@ public class SimulationHamming {
 
 
     public static List<Integer> analyzeResults(String originalMessage, List<Integer> binMessage, List<Integer> encodedMsgBin, List<Integer> transmittedMsgBin,
-                                               String transmittedMsgString, List<Integer> decodedMsgBin, String decodedMsgString) {
+                                               List<Integer> decodedMsgBin, String decodedMsgString) {
         // Analiza bitów po transmisji
         int bitsAfterTransmission = countDifferentBits(encodedMsgBin, transmittedMsgBin);
 
         // Analiza bitów po korekcji
         int bitsAfterCorrection = countDifferentBits(binMessage, decodedMsgBin);
 
-        // Analiza znaków po transmisji
-        int messageAfterTransmission = countDifferentChars(originalMessage, transmittedMsgString);
-
         // Analiza znaków po korekcji
         int messageAfterCorrection = countDifferentChars(originalMessage, decodedMsgString);
 
         // Obliczenie procentowej poprawy dla bitów
         double percentBitsCorrection = ((double) bitsAfterTransmission - bitsAfterCorrection) / bitsAfterTransmission * 100;
-
-        // Obliczenie procentowej poprawy dla znaków
-        double percentCharsCorrection = ((double) messageAfterTransmission - messageAfterCorrection) / messageAfterTransmission * 100;
 
         // Obliczenie ile procent błędu zostało - ostateczny wynik dla bitów
         double percentBitsError = ((double) bitsAfterCorrection) / binMessage.size() * 100;
@@ -149,10 +130,8 @@ public class SimulationHamming {
         List<Integer> results = new ArrayList<>();
         results.add(bitsAfterTransmission);
         results.add(bitsAfterCorrection);
-        results.add(messageAfterTransmission);
         results.add(messageAfterCorrection);
         results.add((int) percentBitsCorrection);
-        results.add((int) percentCharsCorrection);
         results.add((int) percentBitsError);
         results.add((int) percentCharsError);
         return results;
