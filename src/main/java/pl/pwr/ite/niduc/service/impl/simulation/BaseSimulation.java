272c4444dd9a50f93c49abe86f1a4a6a01b76740
Krystyna Nowak <krystyna.nowak003@gmail.com>
2024-05-17 10:15:39 +0200

    usunięcie powtórzeń i naprawa bitR

---
+++ src/main/java/pl/pwr/ite/niduc/service/impl/simulation/BaseSimulation.java
@@ -0,0 +1,85 @@
+package pl.pwr.ite.niduc.service.impl.simulation;
+
+import pl.pwr.ite.niduc.service.impl.codes.BitRepetition;
+import pl.pwr.ite.niduc.service.impl.codes.Hamming;
+import pl.pwr.ite.niduc.service.impl.channels.BSC;
+import pl.pwr.ite.niduc.service.impl.channels.GilbertElliot;
+import pl.pwr.ite.niduc.service.impl.generator.NumberGeneratorImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseSimulation {
+
+    public static List<Double> simulateBitRepetitionBSC(String originalMsgString, int repetitionFactor, double ber) {
+        List<Integer> originalMsgBin = BitRepetition.strToBin(originalMsgString);
+        List<Integer> encodedMsgBin = BitRepetition.encode(originalMsgBin, repetitionFactor);
+        List<Integer> transmittedMsgBin = transmitBSC(encodedMsgBin, ber);
+        List<Integer> decodedMsgBin = BitRepetition.decode(transmittedMsgBin, repetitionFactor);
+
+        return analyzeResults(originalMsgBin, encodedMsgBin, transmittedMsgBin, decodedMsgBin);
+    }
+
+    public static List<Double> simulateBitRepetitionGilbert(String originalMsgString, int repetitionFactor, double pGoodToBad, double pBadToGood, double pErrorWhenGood, double pErrorWhenBad) {
+        List<Integer> originalMsgBin = BitRepetition.strToBin(originalMsgString);
+        List<Integer> encodedMsgBin = BitRepetition.encode(originalMsgBin, repetitionFactor);
+        List<Integer> transmittedMsgBin = transmitGE(encodedMsgBin, pGoodToBad, pBadToGood, pErrorWhenGood, pErrorWhenBad);
+        List<Integer> decodedMsgBin = BitRepetition.decode(transmittedMsgBin, repetitionFactor);
+
+        return analyzeResults(originalMsgBin, encodedMsgBin, transmittedMsgBin, decodedMsgBin);
+    }
+
+    public static List<Double> simulateHamming(String originalMsgString, double ber) {
+        List<Integer> originalMsgBin = Hamming.strToBin(originalMsgString);
+        List<Integer> encodedMsgBin = Hamming.encode(originalMsgBin);
+        List<Integer> transmittedMsgBin = transmitBSC(encodedMsgBin, ber);
+        List<Integer> syndrome = Hamming.syndrome(transmittedMsgBin);
+        List<Integer> correctedMsgBin = Hamming.correct(syndrome, transmittedMsgBin);
+        List<Integer> decodedMsgBin = Hamming.decode(correctedMsgBin);
+
+        return analyzeResults(originalMsgBin, encodedMsgBin, transmittedMsgBin, decodedMsgBin);
+    }
+
+    public static List<Double> simulateHammingGilbert(String originalMsgString, double pGoodToBad, double pBadToGood, double pErrorWhenGood, double pErrorWhenBad) {
+        List<Integer> originalMsgBin = Hamming.strToBin(originalMsgString);
+        List<Integer> encodedMsgBin = Hamming.encode(originalMsgBin);
+        List<Integer> transmittedMsgBin = transmitGE(encodedMsgBin, pGoodToBad, pBadToGood, pErrorWhenGood, pErrorWhenBad);
+        List<Integer> syndrome = Hamming.syndrome(transmittedMsgBin);
+        List<Integer> correctedMsgBin = Hamming.correct(syndrome, transmittedMsgBin);
+        List<Integer> decodedMsgBin = Hamming.decode(correctedMsgBin);
+
+        return analyzeResults(originalMsgBin, encodedMsgBin, transmittedMsgBin, decodedMsgBin);
+    }
+
+    private static List<Integer> transmitBSC(List<Integer> encodedMsg, double ber) {
+        BSC bsc = new BSC(ber, new NumberGeneratorImpl());
+        return Hamming.transmitBsc(encodedMsg, bsc);
+    }
+
+    private static List<Integer> transmitGE(List<Integer> encodedMsg, double pGoodToBad, double pBadToGood, double pErrorWhenGood, double pErrorWhenBad) {
+        GilbertElliot gilbertElliot = new GilbertElliot(pErrorWhenGood, pGoodToBad, pErrorWhenBad, pBadToGood, new NumberGeneratorImpl());
+        return BitRepetition.transmitGe(encodedMsg, gilbertElliot);
+    }
+
+    private static List<Double> analyzeResults(List<Integer> binMessage, List<Integer> encodedMsgBin, List<Integer> transmittedMsgBin, List<Integer> decodedMsgBin) {
+        int bitsAfterTransmission = countDifferentBits(encodedMsgBin, transmittedMsgBin);
+        double percentBitsAfterTransmission = (double) bitsAfterTransmission / encodedMsgBin.size() * 100;
+        int bitsAfterCorrection = countDifferentBits(binMessage, decodedMsgBin);
+        double percentBitsCorrection = ((double) bitsAfterTransmission - bitsAfterCorrection) / bitsAfterTransmission * 100;
+
+        List<Double> results = new ArrayList<>();
+        results.add(percentBitsAfterTransmission);
+        results.add(percentBitsCorrection);
+        return results;
+    }
+
+    private static int countDifferentBits(List<Integer> original, List<Integer> received) {
+        int count = 0;
+        for (int i = 0; i < original.size(); i++) {
+            if (!original.get(i).equals(received.get(i))) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
--- src/main/java/pl/pwr/ite/niduc/service/impl/simulation/SimulationHammingGilbert.java
+++ src/main/java/pl/pwr/ite/niduc/service/impl/simulation/SimulationHammingGilbert.java
@@ -7,7 +7,7 @@ import org.knowm.xchart.XYChartBuilder;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SimulationHamming_BSC {
+public class SimulationHammingGilbert {
 
     public static void main(String[] args) {
         String message = "this is important information that should be kept secret";
@@ -16,32 +16,42 @@ public class SimulationHamming_BSC {
         List<Double> avgPercentBitsCorrectionValues = new ArrayList<>();
         List<Double> avgPercentBitsAfterTransmissionValues = new ArrayList<>();
 
-        for (double ber = 0.05; ber <= 1.0; ber += 0.05) {
+        double pGoodToBad = 0.05;
+        double pBadToGood = 1.0;
+        double pErrorWhenGood = 0.05;
+        double pErrorWhenBad = 0.05;
+
+        while(pGoodToBad <= 1.0 && pBadToGood >= 0.05 && pErrorWhenGood <= 1.0 && pErrorWhenBad <= 1.0) {
 
             // Inicjalizacja sum dla obliczenia średnich
             double totalPercentBitsAfterTransmission = 0;
             double totalPercentBitsCorrection = 0;
 
             for (int i = 0; i < numSimulations; i++) {
-                List<Double> results = BaseSimulation.simulateHamming(message, ber);
+                List<Double> results = BaseSimulation.simulateHammingGilbert(message, pGoodToBad, pBadToGood, pErrorWhenGood, pErrorWhenBad);
 
                 totalPercentBitsAfterTransmission += results.get(0);
                 totalPercentBitsCorrection += results.get(1);
             }
-
             double avgPercentBitsAfterTransmission = totalPercentBitsAfterTransmission / numSimulations;
             double avgPercentBitsCorrection = totalPercentBitsCorrection / numSimulations;
 
             avgPercentBitsAfterTransmissionValues.add(avgPercentBitsAfterTransmission);
             avgPercentBitsCorrectionValues.add(avgPercentBitsCorrection);
 
-            System.out.println("Paremetr ber: " + ber);
+            System.out.println("Paremetr bledow: " + pGoodToBad + ", " + pBadToGood + ", " + pErrorWhenGood + ", " + pErrorWhenBad);
             System.out.println("Procent bledu po transmisji: " + avgPercentBitsAfterTransmission + "%");
             System.out.println("Procent zdolnosci naprawiania bledow: " + avgPercentBitsCorrection + "%");
+            System.out.println();
+
+            pGoodToBad += 0.05;
+            pBadToGood -= 0.05;
+            pErrorWhenGood += 0.05;
+            pErrorWhenBad += 0.05;
         }
 
         // Wyświetlanie wykresu
-        XYChart chart = new XYChartBuilder().width(800).height(600).title("Wykres dla kodu Hamminga i kanalu BSC")
+        XYChart chart = new XYChartBuilder().width(800).height(600).title("Wykres dla kodu Hamminga i kanalu Gilberta-Elliota")
                 .xAxisTitle("Zawartosc bledu [%]").yAxisTitle("Zdolnosc korekcyjna [%]").build();
 
         chart.addSeries("Correction Capability", avgPercentBitsAfterTransmissionValues, avgPercentBitsCorrectionValues);
